b1c7e38e9c73a603f1d9dce0d1a3090fdf060882
Oleksandr Babak <alexanderbabak@proton.me>
2026-08-25 16:12:07 +0200

    feat: c-g nvim fish prompt

--- Config/fish/functions/fish_user_key_bindings.fish
+++ Config/fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,7 @@
 function fish_user_key_bindings
   bind ctrl-space -M insert accept-autosuggestion
+  for mode in insert default visual
+    bind -M $mode ctrl-g edit_command_buffer
+  end
   fish_vi_key_bindings
 end
